dcde1ee45dafeb1da32818cb798387923700670c
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-06 23:11:09 +0000

    Print all single digits numbers base 10 starting from 0 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers of base 10 starting from 0
+* Return:0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
