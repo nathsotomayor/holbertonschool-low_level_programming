2047b8594c02691f86a1a4a6bf30ffb51c6df99f
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-06 19:28:46 +0000

    Print number n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,17 +3,15 @@
 #include <time.h>
 
 /**
-* main print de number n
-* Return: 0
+* main - print number n
+* Return:0
 */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	if (n == 0)
