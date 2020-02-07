56c0fb525f1af39cc3ed4cdbdc327db66572fb34
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-07 00:01:11 +0000

    Prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Print all possible combinations of single-digit numbers
+* Return:0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
