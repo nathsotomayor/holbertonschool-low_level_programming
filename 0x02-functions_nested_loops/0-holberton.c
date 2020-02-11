bb6c7b3d7cdda9c8b8b1db17fe8a7cf42eccd3f2
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-11 23:40:29 +0000

    Prints 'Holberton'

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Prints Holberton
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+
+	return (0);
+}
