45a90eeb27dbf2d50463dd8d3b50f04caaa6fc8a
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-12 02:50:12 +0000

    Prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: int
+ * Return: 1, 0, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
