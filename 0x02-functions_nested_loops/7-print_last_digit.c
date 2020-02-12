4558bbcbdc89fe9002af5c7d6352ed2f08327aea
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-12 04:05:35 +0000

    Prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer
+ * Return: value n
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	_putchar(n + '0');
+
+	return (n);
+}
