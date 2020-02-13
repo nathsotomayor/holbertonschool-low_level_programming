f7bbec24eb0a0b5eb5f83efa364ebffa261d9abc
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-13 16:02:21 +0000

    Multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer.
+ * @a: second integer.
+ * Return: i.
+ */
+
+int mul(int a, int b)
+{
+	int i = a * b;
+
+	return (i);
+}
