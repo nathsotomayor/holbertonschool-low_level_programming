85901ef2a0455de9e7d4e32ada280c493df64c9e
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-19 03:11:09 +0000

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - Prints a string
+ * @str: char
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(*(str + x));
+	}
+	_putchar('\n');
+}
