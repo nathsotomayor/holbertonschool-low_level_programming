7984cc7458e77e8eabde28c3a8b4588c88ff3e78
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-21 05:02:47 +0000

    Reverses the content of an array  of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integersg
+ * @a: int
+ * @n: int
+ * @tmp
+ * Return: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i;
+	n--;
+
+	for (i = 0; i < n; i++, n--)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
