54dac24e161e16e89029ca68f02c2c9720fc6132
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-24 20:31:35 +0000

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - Copies memory area
+ * @src: Pointer to memory area
+ * @dest: Pointer where copy memory area from src
+ * @n: Bytes of the memory area
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int l;
+
+	for (l = 0; l < n && src[l] != '\0'; l++)
+	{
+		dest[l] = src[l];
+	}
+
+	return (dest);
+
+}
