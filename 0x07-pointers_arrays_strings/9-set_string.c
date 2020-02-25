31aeda31b935a96239c8396f66f2cb3b32c7c58a
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-25 05:13:56 +0000

    Sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - Searches a string for any of a set of bytes
+ * @s: Char pointer to pointer
+ * @to: Char pointer
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
