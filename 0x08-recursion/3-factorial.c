e43881c49474a238bc0512fb1eda8e4ee55d4512
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-02-27 00:28:26 +0000

    Returns the factorial of a given numberm

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * factorial - Returns the factorial of a given number
+ * @n: number to calculate the factorial
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+		return (-1);
+
+	return (n * factorial(n - 1));
+}
