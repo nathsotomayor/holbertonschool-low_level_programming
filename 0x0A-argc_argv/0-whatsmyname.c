ab3e40d72ddcf6ada6d92fc9d40c540b63456188
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-01 19:03:32 +0000

    Prints name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main -Prints name of program
+ * @argc: Number of command line arguments
+ * @argv: Array containing the program command line arguments
+ * @i: Iterator
+ * Return: 0 Always success
+ */
+
+int main (int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
