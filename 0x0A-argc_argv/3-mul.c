95d09ceda3d6e58c0c58a4cf53ac50eefd1eef46
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-01 23:49:36 +0000

    Multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * main - Multiplies two numbers
+ * @argc: Number of command line arguments
+ * @argv: Array containing the program command line arguments
+ * @i
+ * Return: 0 Always success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+	}
+	printf("%d\n", mul);
+
+	return (0);
+}
