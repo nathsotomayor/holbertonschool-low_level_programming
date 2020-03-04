067e6b8085e26ae4649939e033eeaa93798c62f7
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-04 00:35:32 +0000

    Creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - Create an array of chars
+ * @size: Size of the array
+ * @c: Char of the array
+ * Return: 0 success or NULL error
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	array = malloc(size * sizeof(char));
+
+	if (size == 0 && !array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
