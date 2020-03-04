f31106b77575bc4d761568f66442077902538fb0
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-04 00:38:50 +0000

    Creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,10 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(size * sizeof(char));
 
-	if (size == 0 && !array)
+	if (size == 0)
 		return (NULL);
+	if (!array)
+		return(NULL);
 
 	for (i = 0; i < size; i++)
 	{
