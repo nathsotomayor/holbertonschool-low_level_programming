515b9a7805b2febf682f8e7fe4aaa0656d7d6f01
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-04 01:33:34 +0000

    Returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Returns the length of a string
+ * @s: char
+ * @x
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+        int x;
+
+        for (x = 0; s[x] != '\0'; )
+        {
+                x++;
+        }
+
+        return (x);
+}
+
+/**
  * *_strdup - Returns a pointer to a newly allocated space in memory
  * @str: Array
  * Return: 0 success or NULL error
