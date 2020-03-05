6eba69711e8fdb21a4b9b047a125406feaa1b771
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-05 20:31:20 +0000

    Concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: char
+ * @x
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; )
+	{
+		x++;
+	}
+
+	return (x);
+}
+
+/**
+ * *string_nconcat - Concatenates two strings
+ * @s1: First array to concatenate
+ * @s2: Second array to concatenate with s1
+ * @n: Numbers of elements from s2 to concatenate
+ * Return: Char if success, NULL if fail
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1, size2, l1, l2;
+	char *sentence;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+
+	sentence = malloc(l1 + l2 + 1 * sizeof(char));
+
+	if (!sentence)
+		return (NULL);
+
+	for (size1 = 0; size1 < l1; size1++)
+	{
+		sentence[size1] = s1[size1];
+	}
+
+	if (n >= l2)
+	{
+		for (size2 = 0; size2 <= l2; size2++)
+		{
+			sentence[size1 + size2] = s1[size2];
+		}
+	}
+	else
+	{
+		for (size2 = 0; size2 <= l2 && size2 < n; size2++)
+		{
+			sentence[size1 + size2] = s2[size2];
+		}
+	}
+
+	return (sentence);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
