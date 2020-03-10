4bcbd3310d090c90aef1a18417f577cba1d25b36
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-10 01:14:49 +0000

    Define a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct 'dog'
+ */
+
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
