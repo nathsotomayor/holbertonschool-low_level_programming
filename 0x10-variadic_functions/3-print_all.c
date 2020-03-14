26714c7479fe3f63dad8b86399e8fcc132f0b673
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-14 01:00:14 +0000

    Prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include "variadic_functions.h"
+
+/**
+ * f_char - Prints char
+ * @lst: List of arguments
+ */
+void f_char(va_list lst)
+{
+	printf("%c", va_arg(lst, int));
+}
+
+/**
+ * f_int - Prints int
+ * @lst: List of arguments
+ */
+void f_int(va_list lst)
+{
+	printf("%i", va_arg(lst, int));
+}
+
+/**
+ * f_float - Prints float
+ * @lst: List of arguments
+ */
+void f_float(va_list lst)
+{
+	printf("%f", va_arg(lst, double));
+}
+
+/**
+ * f_string - Prints string
+ * @lst: List of arguments
+ */
+void f_string(va_list lst)
+{
+	printf("%s", va_arg(lst, char *));
+}
+
+/**
+ * print_all - Prints anything
+ * @format: List of types of arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	t_fmt types[] = {
+		{"c", f_char},
+		{"i", f_int},
+		{"f", f_float},
+		{"s", f_string},
+		{NULL, NULL}
+	};
+
+	int i, j = 0;
+	va_list lst;
+	char *sep = "";
+
+	va_start(lst, format);
+
+	while (format && format[j])
+	{
+		i = 0;
+
+		while (types[i].f != NULL)
+		{
+			if (types[i].type[0] == format[j])
+			{
+				printf("%s", sep);
+				types[i].f(lst);
+				sep = ", ";
+				break;
+			}
+			i++;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(lst);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,30 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct type_fmt - Struct formats
+ *
+ * @type: Formats
+ * @f: The function associated
+ */
+typedef struct type_fmt
+{
+    char *type;
+    void (*f)(va_list);
+} t_fmt;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void f_char(va_list lst);
+void f_int(va_list lst);
+void f_float(va_list lst);
+void f_string(va_list lst);
 
 #endif
