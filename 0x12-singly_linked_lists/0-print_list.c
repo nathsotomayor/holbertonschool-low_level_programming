504171c0ffd06baf1e0c338d7c29a1888513ee3f
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-20 02:54:47 +0000

    Prints all the elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,13 @@ size_t print_list(const list_t *h)
 	for (n = 0; h != '\0'; n++)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] [%s]\n", h->len, h->str);
+		}
 		h = h->next;
 	}
 	return (n);
