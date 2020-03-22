2cf0ad82aa9c1ee5e4b89f89c5cc956a7c7959e8
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-22 20:56:03 +0000

    Prints elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_array - Prints a string
+ * print_array - Prints elements of an array of integers
  * @a: Array of integers
  * @n: Numbers of elements of array
  */
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-			printf("%i",a[i]);
-		else	
-			printf("%i, ",a[i]);
+			printf("%i", a[i]);
+		else
+			printf("%i, ", a[i]);
 	}
 	printf("\n");
 }
