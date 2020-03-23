6f657cb7c41f460c3d882c632e9eae60c55672f8
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-23 21:36:27 +0000

    Free a list, sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free a list.
+ * @head: pointer to head element of list
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *store;
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		store = tmp->next;
+		free(tmp);
+		tmp = store;
+	}
+	*head = NULL;
+}
