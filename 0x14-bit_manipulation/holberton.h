7d6ec2f85e30ad2e8a984c70fb7c8204b4278247
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-27 04:56:34 +0000

    Sets the value of a bit to at a given index

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -8,5 +8,6 @@ unsigned int _pow(int x, unsigned int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
