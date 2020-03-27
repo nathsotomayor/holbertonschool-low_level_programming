8c602f45565fae33bd1d75ee8b7d89a5b99baeef
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-27 00:42:25 +0000

    Converts a binary number to an unsigned int

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-unsigned int _pow(int base, unsigned int power);
+unsigned int _pow(int x, unsigned int y);
 unsigned int binary_to_uint(const char *b);
 
 #endif
