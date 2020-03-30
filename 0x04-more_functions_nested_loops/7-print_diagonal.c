f3e4efcce2e6d37138384837b244af61a10733cb
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-30 22:51:31 +0000

    Draws a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+void print_diagonal(int n)
+{
+        int i, j;
+
+        if (n <= 0)
+                _putchar('\n');
+
+        for (i = 0; i < n; i++)
+        {
+                for (j = 0; j < i; j++)
+                {
+                        _putchar(32);
+                }
+                _putchar(92);
+                _putchar('\n');
+        }
+}
