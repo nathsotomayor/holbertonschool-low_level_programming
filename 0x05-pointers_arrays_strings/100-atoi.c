c417f570774b1f400d589e9506dc0abcde46ca43
Nathaly <nathysoto12.07@gmail.com>
2020-04-21 02:20:15 +0000

    Function that convert a string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Function that convert string to int
+ * @arr: String to convert to int
+ *
+ * Return: Value of converted number
+ */
+
+int _atoi(char *arr)
+{
+	int i = 0, convert = 0;
+
+	for (i = 0; arr[i] != '\0'; ++i)
+	{
+		convert = convert * 10 + arr[i] - '0';
+	}
+
+	return (convert);
+}
