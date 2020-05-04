3800560613d7bfa76d9abd24427646172c73b46b
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-04 04:42:38 +0000

    Adds a new node at the beginning of a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,14 +14,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 
-	if (new != NULL)
-	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
-	}
-	else
+	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
 
 	return (new);
 }
