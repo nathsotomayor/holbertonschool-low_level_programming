a128bd61d29449566afc85a31a3a3b575977693b
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-30 04:09:20 +0000

    Gets the index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash, index;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
+}
