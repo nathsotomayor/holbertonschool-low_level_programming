2ad13a8a575a7dcbf0ec94b887d642fd44524748
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-30 04:25:57 +0000

    Fix betty style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Gets the index of a key 
+ * key_index - Gets the index of a key
  * @key: Key of the value
  * @size: Size of the array of the hash table
  *
- * Return: The index 
+ * Return: The index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
