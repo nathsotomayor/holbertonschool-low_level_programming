613d37d5b6b13c5328d9f0c8cfb8f46f44bbee9d
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-27 19:31:18 -0500

    Create header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGORITHMS_
+#define _SEARCH_ALGORITHMS_
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGORITHMS_ */
